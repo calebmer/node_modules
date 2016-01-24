950ad55c8f512d82afbf5a53abcdec455d598ef6
calebmer <calebmeredith8@gmail.com>
2016-01-23 22:44:25 -0500

    Small refactor

--- packages/vulture-redux/lib/connect.js
+++ packages/vulture-redux/lib/connect.js
@@ -1,9 +1,9 @@
 const defaultMapStateToData = () => ({})
 const defaultMapDispatchToData = dispatch => ({ dispatch })
-const defaultMergeData = (stateData, dispatchData, parentData) => ({
+const defaultMergeData = (stateData, dispatchData, data) => ({
+  ...data,
   ...stateData,
-  ...dispatchData,
-  ...parentData
+  ...dispatchData
 })
 
 export default function connect(
@@ -13,10 +13,15 @@ export default function connect(
 ) {
   return component => function connectedComponent(data, ...args) {
     if (!data || !data.store.dispatch || !data.store.getState) {
-      throw new Error('Function must be passed an object containing a redux store.')
+      throw new Error('To connect to redux, the first argument must be an object with a store property.')
     }
 
     const { store: { getState, dispatch } } = data
-    return this::component(mergeData(mapStateToData(getState()), mapDispatchToData(dispatch), data), ...args)
+    const state = getState()
+    const stateData = mapStateToData(state)
+    const dispatchData = mapDispatchToData(dispatch)
+    const mergedData = mergeData(stateData, dispatchData, data)
+
+    return this::component(mergedData, ...args)
   }
 }
