02eab821b6d82f112da5262284fcda050e87d3a7
calebmer <calebmeredith8@gmail.com>
2016-01-24 08:17:08 -0500

    New script dynamically adds name

--- _scripts/new.sh
+++ _scripts/new.sh
@@ -3,16 +3,16 @@
 name=$1
 root=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)/..
 assets=$root/_assets
-cwd=$(pwd)
+new=$root/$1
 
 cd $root
 
-mkdir $1
+mkdir $new || exit 1
 
-cd $1
+cd $new
 
-cp $assets/.env $cwd/.env
-cp $assets/LICENSE $cwd/LICENSE
-cp -n $assets/README.md $cwd/README.md
-cp -n $assets/package.json $cwd/package.json
-cp -n $assets/index.js $cwd/index.js
+cp $assets/.env $new/.env
+cp $assets/LICENSE $new/LICENSE
+cat $assets/README.md | sed -e "s/\$1/$1/g" > $new/README.md
+cat $assets/package.json | sed -e "s/\$1/$1/g" > $new/package.json
+cp $assets/index.js $new/index.js
